1f03b1afa6bd9d3cd58e0258bea01c50a696b65d
Earl Nobe <earlfunk21@gmail.com>
2025-11-27 14:07:05 +0800

    Update the SEO

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,7 +3,7 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.onserviceit.com',
+      url: 'https://project-7-ruby.vercel.app',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 1,
